7a2aa0b99f3007168f80904f79c6dd4a2844ec56
VDelarosa <vmdel@DESKTOP-QCM166L>
2025-02-09 21:22:46 -0600

    add exercises a and b need to review for loop to output every 3rd integer need to check random array works call methods in main fix overloads?

---
+++ Fundamentals2/Program.cs
@@ -0,0 +1,152 @@
+﻿namespace Fundamentals2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //InputSwitch();
+            ArraysAndLoops();
+        }
+
+        static void InputSwitch()
+        {
+            Console.WriteLine("Enter a W A S D key");
+            string? userInput = Console.ReadLine();
+            string caseSensitive = userInput.ToUpper();
+            switch (caseSensitive)
+            {
+                case "W":
+                    Console.WriteLine("You moved up.");
+                    break;
+                case "A":
+                    Console.WriteLine("You moved left.");
+                    break;
+                case "S":
+                    Console.WriteLine("You moved down.");
+                    break;
+                case "D":
+                    Console.WriteLine("You moved right");
+                    break;
+                default:
+                    Console.WriteLine("You did not move.");
+                    break;
+            }
+        }
+        static void ArraysAndLoops()
+        {
+            int[] allValues = {100, 94, 159, -783, 132, 179, 47, 107, 135, 50};
+            for (int i = allValues.Length; i > 0; i--)
+            {
+                Console.WriteLine(allValues[i - 1]);
+            }
+            bool isInArray = false;
+            while (!isInArray)
+            {
+                Console.WriteLine("Enter a number.");
+                int userNumberInput = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < allValues.Length; i++)
+                {
+                    if (allValues[i] == userNumberInput)
+                    {
+                        isInArray = true;
+                    }
+                }
+            }
+
+            int totalValue = 0;
+            for (int i = 0; i < allValues.Length; i++)
+            {
+                totalValue += allValues[i];
+            }
+            int averageValue = totalValue / allValues.Length;
+            Console.WriteLine("The average value of the array is " + averageValue);
+            for (int i = 0; i < allValues.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(allValues[i]);
+            }
+            for (int i = 0; i < allValues.Length; i++)
+            {
+                if (i % 3 != 0)
+                {
+                    continue;
+                }
+                Console.Write(allValues[i]);
+            }
+            Console.WriteLine("Enter three numbers.");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            int rangeMin = Convert.ToInt32(Console.ReadLine());
+            int rangeMax = Convert.ToInt32(Console.ReadLine());
+            int[] randomArray = new int[arraySize];
+            Random rnd = new Random();
+
+            for(int i = 0; i < randomArray.Length; i++)
+            {
+                randomArray[i] = rnd.Next(rangeMin, rangeMax);
+            }
+        }
+
+        static void OutputArrayInt(int[] exampleArray)
+        {
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine(exampleArray[i]);
+            }
+        }
+        static void Overload(float[] floatArray)
+        {
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine(floatArray[i]);
+            }
+        }
+        static void Overload(double[] doubleArray)
+        {
+            for (int i = 0; i < 0; i++)
+            {
+                Console.WriteLine(doubleArray[i]);
+            }
+        }
+        static void AverageIntToFloat(int[]sampleArray)
+        {
+            float totalValue = 0;
+            for (int i = 0; i < sampleArray.Length; i++)
+            {
+                totalValue += sampleArray[i];
+            }
+            float averageValue = totalValue / Convert.ToSingle(sampleArray.Length);
+            Console.WriteLine("The average value of the array is " + averageValue);
+        }
+        static void AverageOverload(float[]sampleArray)
+        {
+            double totalValue = 0;
+            for (int i = 0; i < sampleArray.Length; i++)
+            {
+                totalValue += sampleArray[i];
+            }
+            double averageValue = totalValue / sampleArray.Length;
+            Console.WriteLine("The average value of the array is " + averageValue);
+        }
+        
+        static void RandomArray()
+        {
+            Console.WriteLine("Enter three numbers.");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            int rangeMin = Convert.ToInt32(Console.ReadLine());
+            int rangeMax = Convert.ToInt32(Console.ReadLine());
+            int[] randomArray = new int[arraySize];
+            Random rnd = new Random();
+
+            for (int i = 0; i < randomArray.Length; i++)
+            {
+                randomArray[i] = rnd.Next(rangeMin, rangeMax);
+            }
+        }
+    }
+}
+
+    
